f1f5b902de7fc28f8d8183fca2a9d66760aff0d6
maskun <maskunflower2@naver.com>
2023-11-10 11:01:34 +0900

    [정인호] 쿼리문버그픽스

--- mall/src/main/java/dao/OrderDao.java
+++ mall/src/main/java/dao/OrderDao.java
@@ -93,7 +93,7 @@ public class OrderDao {
 		String sql = """
 				SELECT o.orders_no orderNo, g.goods_no gooodNo, g.goods_title goodTitle, g.goods_price goodPrice, quantity, total_price totalPrice, orders_state orderStatus, o.createdate createdate, ca.address address, gi.filename filename
 				 FROM orders o INNER JOIN goods g ON o.goods_no = g.goods_no inner join customer_addr ca ON o.customer_addr_no = ca.customer_addr_no INNER JOIN goods_img gi ON g.goods_no = gi.goods_no
-				 WHERE o.customer_no = 2 ORDER BY o.createdate DESC""";
+				 WHERE o.customer_no = ? ORDER BY o.createdate DESC""";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, customerNo);
 		System.out.println(" stmt --> " + stmt);
